41bce77c20aea569eb6110eecb48c41364888286
Brad Gibbs <bradgibbs@mac.com>
2020-02-19 12:16:02 -0600

    path prefixes

--- content/data/index.md
+++
@@ -1,9 +0,0 @@
----
-category: Data
-section: Introduction
-title: Introduction
-description: Introduction to the Data Science documents
-slug: /index
----
-
-. . . text
--- gatsby-config.js
+++ gatsby-config.js
@@ -81,6 +81,9 @@ module.exports = {
           null: [
             'index',
           ],
+          Lifecycle: [
+            'lifecycle',
+          ]
         },
       },
     },
